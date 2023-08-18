757b45170b2e39318acf261bbcc7149c898d967e
wirelessEye <okuloj@outlook.com>
2023-08-18 23:26:37 +1000

    Add api router

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -8,11 +9,27 @@ import (
 	"syscall"
 )
 
+func listFiles(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	dirname, _ := os.UserHomeDir()
+	files, _ := os.ReadDir(dirname)
+
+	var filenames []string
+	for _, e := range files {
+		filenames = append(filenames, e.Name())
+	}
+
+	res, _ := json.Marshal(filenames)
+	w.Write(res)
+}
+
 func main() {
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
 	handler := http.FileServer(http.Dir("web/dist"))
+	http.HandleFunc("/api", listFiles)
 	http.Handle("/", handler)
 	go func() {
 		log.Fatal(http.ListenAndServe(":4331", nil))
