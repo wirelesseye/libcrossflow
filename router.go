d0c3d7c3da407c4af8ed34ba078b2e232048b938
wirelessEye <okuloj@outlook.com>
2023-11-29 13:29:53 +0800

    Router: fix web routing

--- router.go
+++ router.go
@@ -11,18 +11,9 @@ import (
 )
 
 func GetRouter() http.Handler {
-	var resDir string
-	if os.Getenv("APP_ENV") == "dev" {
-		resDir = "web/dist"
-	} else {
-		ex, _ := os.Executable()
-		exPath := filepath.Dir(ex)
-		resDir = filepath.Join(exPath, "res")
-	}
-
 	r := mux.NewRouter()
 	api.HandleAPI(r)
-	r.PathPrefix("/").Handler(http.FileServer(http.Dir(resDir)))
+	r.PathPrefix("/").HandlerFunc(handleWeb)
 
 	if os.Getenv("APP_ENV") == "dev" {
 		headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
@@ -32,4 +23,36 @@ func GetRouter() http.Handler {
 	} else {
 		return r
 	}
+}
+
+func handleWeb(w http.ResponseWriter, r *http.Request) {
+	var resDir string
+	if os.Getenv("APP_ENV") == "dev" {
+		resDir = "web/dist"
+	} else {
+		ex, _ := os.Executable()
+		exPath := filepath.Dir(ex)
+		resDir = filepath.Join(exPath, "res")
+	}
+
+	// Join internally call path.Clean to prevent directory traversal
+	path := filepath.Join(resDir, r.URL.Path)
+
+	// check whether a file exists or is a directory at the given path
+	fi, err := os.Stat(path)
+	if os.IsNotExist(err) || fi.IsDir() {
+		// file does not exist or path is a directory, serve index.html
+		http.ServeFile(w, r, filepath.Join(resDir, "index.html"))
+		return
+	}
+
+	if err != nil {
+		// if we got an error (that wasn't that the file doesn't exist) stating the
+		// file, return a 500 internal server error and stop
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// otherwise, use http.FileServer to serve the static file
+	http.FileServer(http.Dir(resDir)).ServeHTTP(w, r)
 }
